aa96792d53bf4590f0e8ce47bfd6ccf3ac703a73
cmondor <christopher.mondor@verizondigitalmedia.com>
2018-07-19 00:21:32 -0700

    fix terraform_init tests

--- scripts/terraform_init.sh
+++ scripts/terraform_init.sh
@@ -20,24 +20,24 @@ fi
 # https://www.terraform.io/docs/configuration/override.html
 OVERRIDES_DIR=overrides
 OVERRIDES_PATH="${OVERRIDES_DIR}/$1_$2.tf" #toolkitconvetion
-[ -e override.tf ] && rm override.tf
+[ -f override.tf ] && rm override.tf
 [ -d ${OVERRIDES_DIR} ] && {
-    [ -e ${OVERRIDES_PATH} ] && {
+    [ -f ${OVERRIDES_PATH} ] && {
         echo "Linking overrides:  ./${OVERRIDES_PATH} -> ./override.tf"
         ln -s ${OVERRIDES_PATH} override.tf
     }
 }
 
 GLOBALS_PATH="../global.tfvars" #toolkitconvetion
-[ -e global.auto.tfvars ] && rm global.auto.tfvars
-[ -e ../../${GLOBALS_PATH} ] && {
+[ -f global.auto.tfvars ] && rm global.auto.tfvars
+[ -f ../../${GLOBALS_PATH} ] && {
     echo "Linking Globals:  ${GLOBALS_PATH} -> ./global.auto.tfvars"
     ln -s ${OVERRIDES_PATH} global.auto.tfvars
 }
 
 SUPER_GLOBALS_PATH="../../super_global.tfvars" #toolkitconvetion
-[ -e super_global.auto.tfvars ] && rm super_global.auto.tfvars
-[ -e ../../${SUPER_GLOBALS_PATH} ] && {
+[ -f super_global.auto.tfvars ] && rm super_global.auto.tfvars
+[ -f ../../${SUPER_GLOBALS_PATH} ] && {
     echo "Linking Super Globals:  ${SUPER_GLOBALS_PATH} -> ./global.auto.tfvars"
     ln -s ${SUPER_GLOBALS_PATH} super_global.auto.tfvars
 }
